3933f3367bdeb6d9da219bf1f911efe17575b8d0
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2023-09-26 01:48:11 -0400

    feature(main): implement onboarding page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,27 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss.Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
 			'body': ['Mukta']
 		},
-		extend: {},
+		extend: {
+			animation: {
+				'colorized-ping': 'colorized-ping 12s ease-in-out infinite',
+			},
+			keyframes: {
+				'colorized-ping': {
+					'0%': { color: colors.blue[500] },
+					'25%': { color: colors.orange[500] },
+					'50%': { color: colors.emerald[500] },
+					'75%': { color: colors.red[500] },
+					'100%': { color: colors.blue[500] },
+				}
+			}
+		},
 	},
 	plugins: [],
 }
