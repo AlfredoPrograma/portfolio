215d2d3cefb245d84d9a0ec6a655bdb1ef8b8e87
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-03-24 01:36:01 -0400

    feature: add alias colors and update spacing between elements

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
 			body: ["Outfit Variable", "sans-serif"],
 		},
-		extend: {},
+		extend: {
+			colors: {
+				content: "#374151", // text-gray-700, 
+				primary: "#4338ca" // indigo-700
+			}
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography')
