841f9830b116665f5d2220594f547187acf503cf
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2024-04-05 10:34:27 -0400

    feature: add custom styles for inline code blocks in mdx

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -11,6 +11,27 @@ export default {
 			colors: {
 				content: "#374151", // text-gray-700, 
 				primary: "#4338ca" // indigo-700
+			},
+			typography(theme) {
+				return {
+					DEFAULT: {
+						css: {
+							"code::before": {
+								content: 'none'
+							},
+							"code::after": {
+								content: 'none'
+							},
+							code: {
+								color: theme("colors.primary"),
+								backgroundColor: theme("colors.gray.200"),
+								borderRadius: theme("borderRadius.DEFAULT"),
+								paddingInline: theme("spacing[1.5]"),
+								paddingBlock: theme("spacing[0.5]")
+							}
+						}
+					}
+				}
 			}
 		},
 	},
