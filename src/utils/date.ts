146a26a577213489c223439e57c5c8ec5a7a9be5
Alfredo Arvelaez <alfredoprograma.dev@gmail.com>
2025-06-28 18:17:00 -0400

    rollback: remove multilingual support

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,18 +1,3 @@
-import type { Language } from "@i18n/utils"
-
-const englishDateFormatter = new Intl.DateTimeFormat("en-US", {
+export const dateFormatter = new Intl.DateTimeFormat("en-US", {
   dateStyle: "long"
 })
-
-const spanishDateFormatter = new Intl.DateTimeFormat("es-VE", {
-  dateStyle: "long"
-})
-
-export function formatDate(date: Date, lang: Language) {
-  if (lang === "es") {
-    return spanishDateFormatter.format(date)
-  }
-
-  return englishDateFormatter.format(date)
-}
-
